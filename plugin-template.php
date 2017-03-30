b70a2bde9cd9a875138bbd40e133d8d8f613daf0
JayGervais <mrjaygervais@gmail.com>
2017-03-30 16:20:27 -0600

    Basic WordPress Functions
    Basic scripts for WordPress plugin and WooCommerce

---
+++ plugin-template.php
@@ -0,0 +1,15 @@
+<?php
+   /*
+   Plugin Name: 
+   Plugin URI: 
+   Description: 
+   Version: 1.0
+   Author: 
+   Author URI: 
+   License: N/A
+   */
+   
+# plugin code here:   
+
+
+?>
---
+++ sale-percent.php
@@ -0,0 +1,10 @@
+<?php
+// Add save percent next to sale item prices
+
+add_filter( 'woocommerce_sale_price_html', 'woocommerce_custom_sales_price', 10, 2 );
+function woocommerce_custom_sales_price( $price, $product ) {
+	$percentage = round( ( ( $product->regular_price - $product->sale_price ) / $product->regular_price ) * 100 );
+	return $price . sprintf( __(' Save %s', 'woocommerce' ), $percentage . '%' );
+}
+
+?>
